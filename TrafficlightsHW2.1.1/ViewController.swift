7b7ead902c9385a137b2351603eba289d4696144
Maksim Gurkov <maksim.gurkov@gmail.com>
2022-03-24 10:11:15 +0300

    Initial Commit

---
+++ TrafficlightsHW2.1.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrafficlightsHW2.1.1
+//
+//  Created by Максим Гурков on 24.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
